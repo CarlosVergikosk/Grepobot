118fe262fb4086fcd7e34b1c29906f1e0f9db417
Carlos <carlos-vergikosk@live.com.pt>
2022-11-28 20:27:33 +0100

    tag

--- TamperMonkey.js
+++ TamperMonkey.js
@@ -14,10 +14,10 @@
   link.rel = 'stylesheet';
   script.src =
     location.protocol +
-    '//cdn.jsdelivr.net/gh/CarlosVergikosk/Grepobot@0.16/Autobot.js';
+    '//cdn.jsdelivr.net/gh/CarlosVergikosk/Grepobot@0.17/Autobot.js';
   link.href =
     location.protocol +
-    '//cdn.jsdelivr.net/gh/CarlosVergikosk/Grepobot@0.16/Autobot.css';
+    '//cdn.jsdelivr.net/gh/CarlosVergikosk/Grepobot@0.17/Autobot.css';
   head.appendChild(script);
   head.appendChild(link);
   head.setAttribute('xhttps', 1);
